9763b2d646961e81fc298833ab228f3615b63ac3
anton <anton@mfin.co.za>
2018-12-14 09:29:43 +0200

    some things happening
    Signed-off-by: anton <anton@mfin.co.za>

--- index.js
+++ index.js
@@ -1,17 +1,161 @@
 const fetch = require('node-fetch')
+const redis = require("redis")
+const client = redis.createClient()
+const config = require('./config/config.js')
+const db = require('./pg_db').db
+const async = require('async')
 
-let con = 'http://localhost:8332'
-let authHash = 'dXNlcjpwYXNzd29yZA=='
+const con = config.bitcoinURL
+const authHash = config.bitcoinHash
 
+// startup process
+// connect to DB
+// connect to Redis
+// connect to chain, some status call?
 
-getBlockHash(0)
+//async process (1) that gets latest best block height
+  //store block height in Redis
+  //store block height in PostgresDB
 
-function getBlock(blockHash) {
+//async process (2) that gets latest current block height
+  //if current block < max block then we are busy catching up, else we are on our latest block and we can go to sleep? Not sure bitcoin has a subscribe functionality
+
+  //run process for getBlockHash(currentBlockHeight) => hash
+  //.then getBlock(hash)
+  //.then async process (1) store in PostgresDB
+  //      async process (2) store in Redis
+  //      async process (3) map all transactions => transactionHash
+  //        get transaction(transactionHash)
+  //        transaction store in PostgresDB
+
+function start() {
+  console.log("************************************** STARTUP SCRIPT *************************************")
+  updateLatestChainBlock()
+  processBlocks()
+}
+
+function processBlocks() {
+  async.parallel([
+    (callback) => { getLatestChainBlock(callback) },
+    (callback) => { getLatestLocalBlock(callback) }
+  ], (err, blockDetails) => {
+    if(err) {
+      return console.log(err)
+    }
+    let latestChain = blockDetails[0]
+    let latestLocal = blockDetails[1]
+
+    if(!latestLocal) {
+      latestLocal = 0
+    }
+
+    if(latestChain > latestLocal) {
+      console.log("*********************************** PROCESSING NEW BLOCK **********************************")
+      console.log(parseInt(latestLocal) + 1)
+      console.log('*******************************************************************************************')
+
+      getBlockHash(parseInt(latestLocal) + 1, processBlocks)
+    } else {
+      setTimeout(processBlocks, 60000)
+    }
+  })
+}
+
+function setLatestChainBlock(value) {
+  client.set('latest-chain-block', value)
+}
+
+function getLatestChainBlock(callback) {
+  client.get("latest-chain-block", (err, value) => {
+    if(err) {
+      console.log(err)
+    }
+    callback(value)
+  });
+}
+
+function updateLatestChainBlock() {
+  callBitcoin('getblockchaininfo', [], (json) => {
+
+    console.log("************************************ GET BLOCKCHAIN INFO **********************************")
+    console.log(json.blocks)
+    console.log('*******************************************************************************************')
+
+    setLatestChainBlock(json.blocks)
+
+    //call every minute to see if something changed?
+    setTimeout(updateLatestChainBlock, 60000)
+  })
+}
+
+function setLatestLocalBlock(value, callback) {
+  client.set('latest-local-block', value)
+  if(callback) {
+    callback()
+  }
+}
+
+function getLatestLocalBlock(callback) {
+  client.get("latest-local-block", (err, value) => {
+    if(err) {
+      console.log(err)
+    }
+
+    callback(value)
+  });
+}
+
+function getBlockHash(blockNumber, callback) {
+  callBitcoin('getblockhash', [blockNumber], (json) => {
+    getBlock(json.result, callback)
+  })
+}
+
+function getBlock(blockHash, callback) {
+  callBitcoin('getblock', [blockHash], (json) => {
+    //store in DB
+    async.parallel([
+      (callback) => { saveBlock(json, callback) },
+      (callback) => { setLatestLocalBlock(json.height, callback) },
+      (callback) => { getTransactions(json.tx, callback) }
+    ], callback)
+  })
+}
+
+function saveBlock(block, callback) {
+  console.log('*************************************STORING NEW BLOCK*************************************')
+  console.log(block)
+  console.log('*******************************************************************************************')
+  // db.none("insert into blocks () values ();", [])
+  // .then(callback)
+  // .catch(callback)
+}
+
+function getTransactions(transactions, callback) {
+  async.map_limit(trnasactions, 10, getTransaction, callback)
+}
+
+function getTransaction(transaction, callback) {
+  callBitcoin('getTransaction', [transaction], (json) => {
+    saveTransaction(json, callback)
+  })
+}
+
+function saveTransaction(block, callback) {
+  console.log('************************************* STORING NEW TXN *************************************')
+  console.log(block)
+  console.log('*******************************************************************************************')
+  // db.none("insert into transactions () values ();", [])
+  // .then(callback)
+  // .catch(callback)
+}
+
+function callBitcoin(method, params, callback) {
   fetch(con, {
     method: 'POST',
     body: JSON.stringify({
-      "method": "getblock",
-      "params": [blockHash]
+      "method": method,
+      "params": params
     }),
     headers: {
       'Content-Type': 'text/plain;',
@@ -22,30 +166,10 @@ function getBlock(blockHash) {
     return res.json()
   })
   .then((json) => {
-    console.log(json)
-    process.exit(0)
+    callback(json)
   })
   .catch(console.log)
 }
 
-function getBlockHash(blockNumber) {
-  fetch(con, {
-    method: 'POST',
-    body: JSON.stringify({
-      "method": "getblockhash",
-      "params": [blockNumber]
-    }),
-    headers: {
-      'Content-Type': 'text/plain;',
-      'Authorization': 'Basic '+authHash
-    },
-  })
-  .then((res) => {
-    return res.json()
-  })
-  .then((json) => {
-    console.log(json)
-    getBlock(json.result)
-  })
-  .catch(console.log)
-}
+
+start()
